a445206223ff4944b105701eff54add4d40425e9
Student <leilahashurgmail.com>
2023-09-09 17:22:12 +0300

    added  an app on my projectcommit

---
+++ review/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from.models import Comments
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display=("name","comments","ratings","date")
+    
+admin.site.register(Comments,CommentsAdmin)
